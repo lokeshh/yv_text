c8075aa143182f1b1db4922cb3785801417656d4
Lokesh Sharma <lokeshhsharma@gmail.com>
2020-04-26 17:24:36 +0530

    update script

--- update_db_script.py
+++ update_db_script.py
@@ -5,31 +5,18 @@ import re
 
 jsonpickle.set_encoder_options('json', sort_keys=True, indent=2, ensure_ascii=False)
 
-def read_book(n):
-    return jsonpickle.decode(open(f"book_{n}.json").read())
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+import boto3
+
+dynamo = boto3.resource("dynamodb")
+# tbl = dynamo.Table("users")
+verse_table = dynamo.Table('verse')
+count_table = dynamo.Table('count')
+comm_table = dynamo.Table('commentary')
 
 def read_complete_book():
     return jsonpickle.decode(open(f"complete_yv_core.json").read())
-
-def save_book(n, yv_verses):
-    with open(f"book_{n}.json", 'w', encoding='utf-8') as f:
-        f.write(jsonpickle.encode(yv_verses, max_depth=3))
-
-books = {}
-complete_book = []
-for book_number in range(1, 8):
-    books[book_number] = read_book(book_number)
-    complete_book.extend(books[book_number])
-
-# def clean_text(yv_verse):
-#     text = yv_verse.text
-#     clean_text = []
-#     for t in re.split('\[|\'|\]|\\\\n|, ', text):
-#         if t in ['', '<br/>']:
-#             continue
-#         else:
-#             clean_text.append(t.strip())
-#     yv_verse.text = clean_text
 
 def generate_complete(complete_book):
     complete_text = []
@@ -39,15 +26,9 @@ def generate_complete(complete_book):
         complete_text.append("\n")
     return "\n".join(complete_text)
 
-
-# with open('complete.txt', 'w') as f:
-#     f.write(generate_complete(complete_book))
-
-
-breakpoint()
-
 def get_id(v):
     return int(v.book * 1e6 + v.chapter * 1e3 + v.verse)
+
 
 # with tbl.batch_writer(overwrite_by_pkeys=['id']) as batch:
 #     for v in books[7]:
