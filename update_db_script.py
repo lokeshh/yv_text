0377bf1f07d0edb0de806f5913318fcd2d5db4e7
Lokesh Sharma <lokeshhsharma@gmail.com>
2020-08-01 17:09:59 +0530

    till chapter 10

--- update_db_script.py
+++ update_db_script.py
@@ -50,13 +50,13 @@ vlm = read('vlm.json')
 abs = read('abs.json')
 
 for i in vlm:
-    if i.book == 4 and i.chapter in [7]:
+    if i.book == 4 and i.chapter in [9, 10]:
         i.text = [""]
         abs.append(i)
 
 save(abs, 'abs.json')
 
 abs = read('abs.json')
-abs = [i for i in abs if i.chapter == 7]
+abs = [i for i in abs if i.chapter in (9, 10)]
 for v in abs:
     comm_table.put_item(Item = {'id': 10000000 + get_id(v), 'text': v.text, 'author': 'abs'})
