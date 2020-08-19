110c03a3d1df3f2e4e8559ef5e547255580a3548
Lokesh Sharma <lokeshhsharma@gmail.com>
2020-08-19 08:04:04 +0530

    17 done

--- update_db_script.py
+++ update_db_script.py
@@ -50,14 +50,14 @@ vlm = read('vlm.json')
 abs = read('abs.json')
 
 for i in vlm:
-    if i.book == 4 and i.chapter in [15]:
+    if i.book == 4 and i.chapter in [17]:
         i.text = [""]
         abs.append(i)
 
 save(abs, 'abs.json')
 
 abs = read('abs.json')
-abs = [i for i in abs if i.chapter in (15,)]
+abs = [i for i in abs if i.chapter in (17,)]
 for v in abs:
     comm_table.put_item(Item = {'id': 10000000 + get_id(v), 'text': v.text, 'author': 'abs'})
 
