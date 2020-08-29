c2e9af5aca4e4d9fe5450107258cf66f4077d47b
Lokesh Sharma <lokeshhsharma@gmail.com>
2020-08-29 07:59:57 +0530

    till 19

--- update_db_script.py
+++ update_db_script.py
@@ -50,7 +50,7 @@ vlm = read('vlm.json')
 abs = read('abs.json')
 
 for i in vlm:
-    if i.book == 4 and i.chapter in [18]:
+    if i.book == 4 and i.chapter in [19]:
         i.text = [""]
         abs.append(i)
 
