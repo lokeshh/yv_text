1fdb81c7876f7a9faee8d9026dc060cb49ce0a55
Lokesh Sharma <lokeshhsharma@gmail.com>
2021-10-24 07:18:24 +0000

    add chapterwise md file

--- convert_to_md.py
+++ convert_to_md.py
@@ -11,13 +11,14 @@ def save(data, filename):
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(jsonpickle.encode(data, max_depth=3))     
 
-def generate_complete(complete_book):
-    complete_text = []
-    for v in complete_book:
-        complete_text.append(f"{v.book}.{v.chapter}.{v.verse}")
-        complete_text.extend(v.text)
-        complete_text.append("\n")
-    return "\n".join(complete_text)
+def generate_md(data):
+    md_text = []
+    for v in data:
+        md_text.append(f"{v.book}.{v.chapter}.{v.verse}")
+        md_text.extend(v.text)
+        md_text.append("\n")
+    return "\n".join(md_text)
+
 
 def get_id(v):
     return int(v.book * 1e6 + v.chapter * 1e3 + v.verse)
@@ -25,6 +26,16 @@ def get_id(v):
 yv = read('yv_core.json')
 mk = read('mk_core2.json')
 
-txt = generate_complete(mk)
-with open('mokshopaya.md', 'w') as f:
-    f.write(txt)
+for book in range(1, 7):
+    for chapter in range(1,1000):
+        chapter_data = [i for i in mk if i.book == book and i.chapter == chapter]
+        if chapter_data == []:
+            continue
+        md_text = generate_md(chapter_data)
+        with open(f"mk_md/{book}/{chapter}.md", 'w') as f:
+            f.write(md_text)
+
+
+# txt = generate_md(mk)
+# with open('mokshopaya.md', 'w') as f:
+#     f.write(txt)
